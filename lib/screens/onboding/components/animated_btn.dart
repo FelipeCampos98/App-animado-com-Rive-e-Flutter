faa4e5f32711ece8d178e81d06c5bf99a9e4b1ba
Felipe Campos <jedicamposbr@gmail.com>
2023-03-27 15:16:52 -0300

    commit

---
+++ lib/screens/onboding/components/animated_btn.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:rive/rive.dart';
+
+class AnimatedBtn extends StatelessWidget {
+  const AnimatedBtn({
+    Key? key,
+    required RiveAnimationController btnAnimationColtroller,
+    required this.press,
+  })  : _btnAnimationColtroller = btnAnimationColtroller,
+        super(key: key);
+
+  final RiveAnimationController _btnAnimationColtroller;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: SizedBox(
+        height: 64,
+        width: 260,
+        child: Stack(
+          children: [
+            RiveAnimation.asset(
+              "assets/RiveAssets/button.riv",
+              controllers: [_btnAnimationColtroller],
+            ),
+            Positioned.fill(
+              top: 8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(CupertinoIcons.arrow_right),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "start app",
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -2,7 +2,11 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rive/rive.dart';
+
+import 'components/animated_btn.dart';
+import 'components/sign_in_form.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
@@ -69,6 +73,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                       AnimatedBtn(
                         press: () {
                           _btnAnimationColtroller.isActive = true;
+                          customSigninDialog(context);
                         },
                         btnAnimationColtroller: _btnAnimationColtroller,
                       ),
@@ -82,51 +87,127 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       ),
     );
   }
-}
 
-class AnimatedBtn extends StatelessWidget {
-  const AnimatedBtn({
-    Key? key,
-    required RiveAnimationController btnAnimationColtroller,
-    required this.press,
-  })  : _btnAnimationColtroller = btnAnimationColtroller,
-        super(key: key);
-
-  final RiveAnimationController _btnAnimationColtroller;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: SizedBox(
-        height: 64,
-        width: 260,
-        child: Stack(
-          children: [
-            RiveAnimation.asset(
-              "assets/RiveAssets/button.riv",
-              controllers: [_btnAnimationColtroller],
+  Future<Object?> customSigninDialog(BuildContext context) {
+    return showGeneralDialog(
+      barrierDismissible: true,
+      barrierLabel: "Sign In",
+      context: context,
+      pageBuilder: ((context, _, __) => Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              children: [
+                Positioned.fill(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 30,
+                      sigmaY: 30,
+                    ),
+                    child: SizedBox(),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    height: 620,
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    padding: EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.94),
+                      borderRadius: BorderRadius.all(Radius.circular(40)),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Sign-in",
+                            style:
+                                TextStyle(fontSize: 34, fontFamily: "Poppins"),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            child: Text(
+                              "Animated app made with flutter and background animation made in Rive with shapes and blur",
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          const SignInform(),
+                          Row(
+                            children: [
+                              Expanded(child: Divider()),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 16),
+                                child: Text(
+                                  "OR",
+                                  style: TextStyle(
+                                    color: Colors.black26,
+                                  ),
+                                ),
+                              ),
+                              Expanded(child: Divider()),
+                            ],
+                          ),
+                          const Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 24),
+                            child: Text(
+                              "Sign Up with Email, Apple or Google",
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: (() {}),
+                                icon: SvgPicture.asset(
+                                  "assets/icons/email.svg",
+                                  height: 64,
+                                  width: 64,
+                                ),
+                              ),
+                              IconButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: (() {}),
+                                icon: SvgPicture.asset(
+                                  "assets/icons/apple_box.svg",
+                                  height: 64,
+                                  width: 64,
+                                ),
+                              ),
+                              IconButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: (() {}),
+                                icon: SvgPicture.asset(
+                                  "assets/icons/google_box.svg",
+                                  height: 64,
+                                  width: 64,
+                                ),
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: () => Navigator.of(context).pop(),
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: const Padding(
+                                padding: const EdgeInsets.only(top: 24.0),
+                                child: Icon(
+                                  Icons.close,
+                                  color: Colors.black,
+                                  size: 28,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Positioned.fill(
-              top: 8,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(CupertinoIcons.arrow_right),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    "start app",
-                    style: TextStyle(fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
